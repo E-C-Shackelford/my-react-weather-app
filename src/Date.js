63109661768c67a40bca7f502be806f2a1b681e1
E-C-Shackelford <98488905+E-C-Shackelford@users.noreply.github.com>
2022-04-27 13:05:19 -0700

    Add code for getting date and run command for updating dependencies

--- src/Date.js
+++ src/Date.js
@@ -2,10 +2,41 @@ import React from "react";
 
 import "./Date.css";
 
-export default function Date() {
+export default function Date(props) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let currentDay = days[props.date.getDay()];
+  let currentMonth = months[props.date.getMonth()];
+  let currentDate = props.date.getDate();
+
+  let currentHours = props.date.getHours();
+  if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+  }
+  let currentMinutes = props.date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
   return (
     <div className="Date">
-      <h2>Wed Apr 20 | 13:32</h2>
+      <h2>
+        {currentDay} {currentMonth} {currentDate} | {currentHours}:
+        {currentMinutes}
+      </h2>
     </div>
   );
 }
